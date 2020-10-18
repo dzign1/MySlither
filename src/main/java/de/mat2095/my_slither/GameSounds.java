b875c25aff983a61adcfef566f1c41947615cc24
dzign1 <44317715+dzign1@users.noreply.github.com>
2020-10-18 15:36:35 +0100

    Added static methods for food and death sounds

--- src/main/java/de/mat2095/my_slither/GameSounds.java
+++ src/main/java/de/mat2095/my_slither/GameSounds.java
@@ -1,8 +1,49 @@
 package de.mat2095.my_slither;
 
-import javax.sound.sampled.*;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 import java.io.*;
 
 public class GameSounds {
+
+    /**
+     * Plays a death sound
+     */
+    public static void deathSound()
+    {
+        try
+        {
+            String deathSound = "DeathSound.mp3";
+            InputStream in = new FileInputStream(deathSound);
+            AudioStream as = new AudioStream(in);
+
+            AudioPlayer.player.start(as);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * Sound for when player eats bad food
+     */
+    public static void badFood()
+    {
+
+    }
+
+    /**
+     * Sound for when the player's slither eats good food
+     */
+    public static void goodFood()
+    {
+
+    }
+
+
+
 
 }
