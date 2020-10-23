0e034ac74b7505fd9121913d2b9100333ab06c29
dzign1 <44317715+dzign1@users.noreply.github.com>
2020-10-23 14:13:00 +0100

    Added audioplaying methods

--- src/main/java/de/mat2095/my_slither/GameSounds.java
+++ src/main/java/de/mat2095/my_slither/GameSounds.java
@@ -1,7 +1,6 @@
 package de.mat2095.my_slither;
 
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
+import javax.sound.sampled.*;
 import java.io.*;
 
 public class GameSounds {
@@ -13,34 +12,47 @@ public class GameSounds {
     {
         try
         {
-            String deathSound = "DeathSound.mp3";
-            InputStream in = new FileInputStream(deathSound);
-            AudioStream as = new AudioStream(in);
+            File deathSound = new File("src/main/java/de/mat2095/Sounds/DeathSound.wav");
+            System.out.println(deathSound.getAbsolutePath()+deathSound.getCanonicalPath());
+            AudioInputStream as = AudioSystem.getAudioInputStream(deathSound);
+            Clip clip = AudioSystem.getClip();
+            clip.open(as);
+            clip.start();
 
-            AudioPlayer.player.start(as);
+
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
-
-
     }
-
-    /**
-     * Sound for when player eats bad food
-     */
-    public static void badFood()
-    {
-
-    }
-
     /**
      * Sound for when the player's slither eats good food
      */
-    public static void goodFood()
+    public static void eatFood()
     {
+        try
+        {
+            try
+            {
+                File deathSound = new File("src/main/java/de/mat2095/Sounds/FoodSound.wav");
+                System.out.println(deathSound.getAbsolutePath()+deathSound.getCanonicalPath());
+                AudioInputStream as = AudioSystem.getAudioInputStream(deathSound);
+                Clip clip = AudioSystem.getClip();
+                clip.open(as);
+                clip.start();
 
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 
